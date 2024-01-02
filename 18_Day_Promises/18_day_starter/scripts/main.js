3eb63184def6b3ff9848e00ea560e298565d1e88
anna-llorens <annallorens_12@hotmail.com>
2024-01-02 17:08:32 +0100

    Added countries exercices

--- 18_Day_Promises/18_day_starter/scripts/main.js
+++ 18_Day_Promises/18_day_starter/scripts/main.js
@@ -1,2 +1,29 @@
-console.log(countries)
-alert('Open the console and check if the countries has been loaded')
+const url = "https://restcountries.com/v2/all";
+const catsAPI = "https://api.thecatapi.com/v1/breeds";
+
+const fetchData = async () => {
+  try {
+    const response = await fetch(url);
+    const countries = await response.json();
+    const languages = [];
+    return countries.forEach((country) =>
+      country.languages.forEach((language) => {
+        if (!languages.includes(language.name)) {
+          languages.push(language.name);
+        }
+        return console.log(languages);
+      })
+    );
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+const fetchCats = async () => {
+  const response = await fetch(catsAPI);
+  const cats = await response.json();
+  return cats.every((cat) => console.log(cat));
+};
+fetchCats();
+
+fetchData();
